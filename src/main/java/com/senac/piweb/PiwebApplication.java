29377e65d9819e2c4ef9e8a40a7bd01421ae1939
Helena Sokabe <helena.sokabe@hotmail.com>
2023-12-09 09:13:27 -0300

    Criacao do Projeto

---
+++ src/main/java/com/senac/piweb/PiwebApplication.java
@@ -0,0 +1,13 @@
+package com.senac.piweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/piweb/PiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.piweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
